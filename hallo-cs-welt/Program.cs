43b9ef9d333e523b96accbe9945b1e4e43faf345
Markus Hollerbach <markus.hollerbach@codetext.de>
2019-06-18 22:04:16 +0200

    Fügt die leere, initiale Solution hinzu.

---
+++ hallo-cs-welt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hallo_cs_welt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
